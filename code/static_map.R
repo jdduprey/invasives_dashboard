64fcce549fc586abb04addce012ccd39b5f08933
jdduprey <joe.d.duprey@gmail.com>
2022-04-11 12:06:57 -0700

    working static map, very slow

--- code/static_map.R
+++ code/static_map.R
@@ -14,26 +14,30 @@ library(tidyverse)
 library(here)
 library(dplyr)
 
-
+# load shapefiles, metadata
 NW_coast <- read_sf("./data/ne_10m_coastline/ne_10m_coastline.shp")
 site_meta <- read.csv("./data/site_meta.csv")
 invasion_data <- read.csv("./data/monthly_invasion_data.csv")
 
+# link site data with invasives data
 full_map_df <- left_join(invasion_data, site_meta)
 
+# this function will operate with the reactive slider
 time_filter <- function(df, selected_month, selected_year){
   out_df <- df %>%
     filter(month %in% c(selected_month)) %>%
     filter(year %in% c(selected_year))
 }
 
+# and get a single month and year
 mar_2018_df <- time_filter(full_map_df, 03, 2018)
 
 
+# store map with plot to variable
 p <- ggplot() + 
   geom_sf(data=NW_coast, col = 1, fill = "ivory") +
   coord_sf(xlim = -c(125, 122), ylim = c(47,49)) +
-  geom_point(data = mar_2018_df, aes(x = long, y = lat, color=prop_nn), size = 8) +
+  geom_point(data = mar_2018_df, aes(x = long, y = lat, color=prop_nn), size = 8, alpha = 0.8) +
   scale_color_distiller(palette = "RdYlBu") +
   geom_text(data= mar_2018_df, aes(x = long, y = lat, label=round(prop_nn,2)), size = 2) +
   ggrepel::geom_text_repel(data = mar_2018_df, aes(x = long, y = lat, label = site)) +
@@ -42,12 +46,13 @@ p <- ggplot() +
         axis.text = element_text(size = 8, colour = 1, face = "bold"),
         panel.grid = element_line(colour = NA)) 
 
+# save plot
 ggsave(filename="./figures/site_map.png")
+
+# display plot
 p 
 
-p + geom_point(data = site_meta, aes(x = long, y = lat), size = 5, col="red")
 
-p
   #scale_x_continuous(breaks = c(35,55))+
   #scale_y_continuous(breaks = c(-25, 2)) +
   # ggsn::scalebar(location = "bottomleft", x.min = 35, x.max = 60,
@@ -55,7 +60,6 @@ p
   #                model = "WGS84", st.dist = 0.02, st.size = 4)+
   # labs(x = NULL, y = NULL)
 
-#TODO link this with invasion data
 #TODO clip shapefile so it loads reasonably 
 #TODO lower opacity of circles so coast is visible
 
